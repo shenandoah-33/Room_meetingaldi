5991cf8ed2358900ac9e1fa7ab6d38959916061e
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-16 09:40:36 +0700

    register done push by ojan ganteng

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -19,7 +19,6 @@
                 <div class="fw-bold text-center" style="margin-top:115px; ">
                     <p class="text-light">KOMISI PEMILIHAN UMUM REPUBLIK INDONESIA</p>
                 </div>
-<<<<<<< HEAD
 
             </div>
 
@@ -58,7 +57,6 @@
                    <div class="signin">
                     <span> Have Account? <a href="login">Login</a></span>
                    </div>
-=======
             @endif
         <div class="register-box">
             <form action="" method="post">
@@ -84,7 +82,6 @@
                 </div>
                 <div class="text-center text-light">
                     Have Account? <a href="login">Login</a>
->>>>>>> 72099ea1bc0814cc1acb2e046626cb3bcd1d4e46
                 </div>
             </form>
             </div>
@@ -96,7 +93,7 @@
 </html>
 
 
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -179,3 +176,72 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
     </html> --}}
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body>
+  <div class="wrapper">
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 side-image">
+
+                <!-------------      image     ------------->
+
+                <img src="image/logo-kpu.png" alt="">
+                <div class="fw-bold text-center" style="margin-top:115px; ">
+                    <p class="text-light">KOMISI PEMILIHAN UMUM REPUBLIK INDONESIA</p>
+                </div>
+
+            </div>
+
+
+            <div class="col-md-6 right">
+                <div class="input-box">
+
+                            @if (session('status'))
+                                <div class="alert alert-danger">
+                                    {{ session('message') }}
+                                </div>
+                            @endif
+
+                   <header>Register</header>
+                   <form action="/login" method="post">
+                    @csrf
+                   <div class="input-field">
+                        <input type="text" class="input" id="username" required="" autocomplete="off" name="username">
+                        <label for="username">Username</label>
+                    </div>
+                   <div class="input-field">
+                        <input type="password" class="input" id="pass" required="" name="password">
+                        <label for="pass">Password</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="phone" class="input" id="phone" required="" name="phone">
+                        <label for="phone">Phone</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="biro" class="input" id="biro" required="" name="biro">
+                        <label for="biro">Biro</label>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary form-control">Login</button>
+                    </div>
+                   <div class="signin">
+                    <span> Have Account? <a href="/login">Sign In</a></span>
+                   </div>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
