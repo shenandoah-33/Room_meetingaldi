dad353424846926dc1923df040804f7b47be96b1
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-16 10:04:04 +0700

    done all register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,96 +1,4 @@
-{{--<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Login</title>
-</head>
-<body>
-  <div class="wrapper">
-    <div class="container main">
-        <div class="row">
-            <div class="col-md-6 side-image">
-
-                <!-------------      image     ------------->
-
-                <img src="image/logo-kpu.png" alt="">
-                <div class="fw-bold text-center" style="margin-top:115px; ">
-                    <p class="text-light">KOMISI PEMILIHAN UMUM REPUBLIK INDONESIA</p>
-                </div>
-
-            </div>
-
-
-            <div class="col-md-6 right">
-                <div class="input-box">
-
-                            @if (session('status'))
-                                <div class="alert alert-danger">
-                                    {{ session('message') }}
-                                </div>
-                            @endif
-
-                   <header>REGISTER</header>
-                   <form action="/login" method="post">
-                    @csrf
-                   <div class="input-field">
-                        <input type="text" class="input" id="username" required="" autocomplete="off" name="username">
-                        <label for="username">Username</label>
-                    </div>
-                   <div class="input-field">
-                        <input type="password" class="input" id="pass" required="" name="password">
-                        <label for="pass">Password</label>
-                    </div>
-                    <div class="input-field">
-                        <input type="phone" class="input" id="phone" required="" name="phone">
-                        <label for="phone">Phone</label>
-                    </div>
-                    <div class="input-field">
-                        <input type="biro" class="input" id="biro" required="" name="biro">
-                        <label for="biro">Biro</label>
-                    </div>
-                    <div>
-                        <button type="submit" class="btn btn-primary form-control">Register</button>
-                    </div>
-                   <div class="signin">
-                    <span> Have Account? <a href="login">Login</a></span>
-                   </div>
-            @endif
-        <div class="register-box">
-            <form action="" method="post">
-                @csrf
-                <div>
-                    <label for="username" class="form-label text-light">Username</label>
-                    <input type="text" name="username" id="username" class="form-control">
-                </div>
-                <div>
-                    <label for="password" class="form-label text-light">Password</label>
-                    <input type="password" name="password" id="password" class="form-control" required>
-                </div>
-                <div>
-                    <label for="phone" class="form-label text-light">Phone</label>
-                    <input type="text" name="phone" id="phone" class="form-control">
-                </div>
-                <div>
-                    <label for="biro" class="form-label text-light">Biro</label>
-                    <textarea name="biro" id="biro" class="form-control" style="resize: none" required></textarea>
-                </div>
-                <div>
-                    <button type="submit" class="btn btn-primary form-control">Register</button>
-                </div>
-                <div class="text-center text-light">
-                    Have Account? <a href="login">Login</a>
-                </div>
-            </form>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
+{{--<
 
 
 <!DOCTYPE html>
@@ -205,14 +113,24 @@
             <div class="col-md-6 right">
                 <div class="input-box">
 
-                            @if (session('status'))
-                                <div class="alert alert-danger">
-                                    {{ session('message') }}
-                                </div>
-                            @endif
+                    @if ($errors->any())
+                    <div class="alert alert-danger" style="width: 500px">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('message') }}
+                        </div>
+                    @endif
 
                    <header>Register</header>
-                   <form action="/login" method="post">
+                   <form action="/register" method="post">
                     @csrf
                    <div class="input-field">
                         <input type="text" class="input" id="username" required="" autocomplete="off" name="username">
