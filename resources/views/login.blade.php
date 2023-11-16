5f9439294d27abeec4c37b674cd85e686835ca2f
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-16 12:21:12 +0700

    HALAMAN INDEX,LOGIN,REGISTER,NEW CSS. OJAN GANTENG

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -64,7 +64,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.login.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Login</title>
 </head>
@@ -76,7 +76,7 @@
 
                 <!-------------      image     ------------->
 
-                <img src="image/logo-kpu.png" alt="">
+                <img src="img/logo-kpu.png" alt="">
                 <div class="fw-bold text-center" style="margin-top:115px; ">
                     <p class="text-light">KOMISI PEMILIHAN UMUM REPUBLIK INDONESIA</p>
                 </div>
