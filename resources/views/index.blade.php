1ac97d2ecc2ea20cda835742429224a2d106a91c
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-21 08:59:51 +0700

    selasa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,7 +61,7 @@
 
       <!-- Hero -->
       <section class="hero" id="hero">
-        <h1 class="animate_animated animatebackInDown animate_flipInX">Amazing Mountain yang ada di seluruh indonesia</h1>
+        <h1 class="animate_animated animatebackInDown animate_flipInX">KOMISI PEMILIHAN UMUM REPUBLIK INDONESIA</h1>
       </section>
       <!-- Hero -->
     </header>
@@ -158,7 +158,7 @@
     </footer>
     <!-- footer -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="./dist/js/script.js"></script>
+    <script src="./public/js/script.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
       AOS.init();
