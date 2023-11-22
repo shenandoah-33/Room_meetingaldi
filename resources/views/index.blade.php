04a4f91eeba6a82f8b97cbd089de7c1a4b2a5f0c
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-22 15:42:30 +0700

    sore

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -155,11 +155,13 @@
     contact -->
 
     <!-- footer -->
+    <body>
     <footer>
       <p>&copy; 2024 PRABOWO MALAS MALAS MALAS</p>
     </footer>
+    </body>
     <!-- footer -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>    <script src="js/scipt.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="./js/script.js"></script>
     <script>
