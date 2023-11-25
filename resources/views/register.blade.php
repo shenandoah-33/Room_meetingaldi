3f78d6763e31964adc84a809cca685980348d66d
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-25 17:41:08 +0700

    cek  di hari libur

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -92,7 +92,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.register.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Login</title>
+    <title>Register</title>
 </head>
 <body>
   <div class="wrapper">
