baa5eb0c21fd5c89b48c991f082d97dfbfe724a0
fauzan6666 <fauzan.fatoni.salim@gmail.com>
2023-11-27 10:09:37 +0700

    logo title by zans

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <title>HOME</title>
+    <link rel="icon" href="img/logo-kpu.png" type="image/x-icon">
   </head>
   <body>
     <header class="beranda" id="beranda">
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -93,6 +93,7 @@
     <link rel="stylesheet" href="{{asset('css/style.register.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Register</title>
+    <link rel="icon" href="img/logo-kpu.png" type="image/x-icon">
 </head>
 <body>
   <div class="wrapper">
