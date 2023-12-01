1a58b7c5f90de9d3381d0c73defcf61ac135f2f5
dimsky28 <dimasrastafara280602@gmail.com>
2023-12-01 11:03:01 +0700

    Update Profile by Dim's

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Profile')
     
 @section('content')
-    <h1>Profile</h1>
+    <h1>Welcome, {{Auth::user()->username}}</h1>
 
     <div class="mt-5">
         <h1>Your Booking</h1>
